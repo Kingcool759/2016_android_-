bcac35bfcdc8d9310e903547e53de39ef3e389e6
kingcool759 <3058587756@qq.com>
2018-11-26 18:03:41 +0800

    update

---
+++ one-man-src/src-zhaojianwei/YiQiYou/YiQiYou/app/src/main/java/com/kingcool/yiqiyou/LoginActivity.java
@@ -0,0 +1,35 @@
+package com.kingcool.yiqiyou;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Button btn1 = findViewById(R.id.btn_submit);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //从登录界面activity跳转到主界面mainActivity
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btn2 = findViewById(R.id.btn_register);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //从登录界面activity跳转到新用户注册界面RegisterActivity
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ one-man-src/src-zhaojianwei/YiQiYou/YiQiYou/app/src/main/java/com/kingcool/yiqiyou/RegisterActivity.java
@@ -0,0 +1,24 @@
+package com.kingcool.yiqiyou;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        Button btn1 = findViewById(R.id.btn_goginglogin);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
